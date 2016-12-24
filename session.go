hanzoai/xorm

commit 64b3df6e63ae41210f7dc9e2ab71c5413b24e366

bug fixed for temp table name for insert multiple beans

session.go
@@ -2231,7 +2231,7 @@ func (session *Session) Insert(beans ...interface{}) (int64, error) {
 			if size > 0 {
 				if session.Engine.SupportInsertMany() {
 					cnt, err := session.innerInsertMulti(bean)
-					session.resetStatement()
+					//session.resetStatement()
 					if err != nil {
 						return affected, err
 					}
@@ -2239,7 +2239,7 @@ func (session *Session) Insert(beans ...interface{}) (int64, error) {
 				} else {
 					for i := 0; i < size; i++ {
 						cnt, err := session.innerInsert(sliceValue.Index(i).Interface())
-						session.resetStatement()
+						//session.resetStatement()
 						if err != nil {
 							return affected, err
 						}
@@ -2249,7 +2249,7 @@ func (session *Session) Insert(beans ...interface{}) (int64, error) {
 			}
 		} else {
 			cnt, err := session.innerInsert(bean)
-			session.resetStatement()
+			//session.resetStatement()
 			if err != nil {
 				return affected, err
 			}

commit 60894ed4687430aa820852889874f1fc6890b50e

bug fixed for gogits/gogs#3928 introduced by 64b3df6

session.go
@@ -2240,6 +2240,7 @@ func (session *Session) Insert(beans ...interface{}) (int64, error) {
 	if session.IsAutoClose {
 		defer session.Close()
 	}
+	defer session.resetStatement()
 
 	for _, bean := range beans {
 		sliceValue := reflect.Indirect(reflect.ValueOf(bean))
@@ -2248,7 +2249,6 @@ func (session *Session) Insert(beans ...interface{}) (int64, error) {
 			if size > 0 {
 				if session.Engine.SupportInsertMany() {
 					cnt, err := session.innerInsertMulti(bean)
-					//session.resetStatement()
 					if err != nil {
 						return affected, err
 					}
@@ -2256,7 +2256,6 @@ func (session *Session) Insert(beans ...interface{}) (int64, error) {
 				} else {
 					for i := 0; i < size; i++ {
 						cnt, err := session.innerInsert(sliceValue.Index(i).Interface())
-						//session.resetStatement()
 						if err != nil {
 							return affected, err
 						}
@@ -2266,7 +2265,6 @@ func (session *Session) Insert(beans ...interface{}) (int64, error) {
 			}
 		} else {
 			cnt, err := session.innerInsert(bean)
-			//session.resetStatement()
 			if err != nil {
 				return affected, err
 			}