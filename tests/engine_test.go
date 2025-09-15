0c11d8a5b03ed0e29c9429b5179bcb713968fd40
Lunny Xiao <xiaolunwen@gmail.com>
2025-09-15 02:41:04 +0000

    Upgrade pgx (#2534)
    Reviewed-on: https://gitea.com/xorm/xorm/pulls/2534

--- tests/engine_test.go
+++ tests/engine_test.go
@@ -16,7 +16,7 @@ import (
 
 	_ "gitee.com/travelliu/dm"
 	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/jackc/pgx/v4/stdlib"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 	_ "github.com/microsoft/go-mssqldb"
