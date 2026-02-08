ec77bd214b5e2c80f7617d5d3be3e5524da416f2
Lunny Xiao <xiaolunwen@gmail.com>
2026-02-08 03:07:57 +0000

    Support select for update of postgres (#2557)
    Reviewed-on: https://gitea.com/xorm/xorm/pulls/2557

--- internal/statements/query.go
+++ internal/statements/query.go
@@ -253,8 +253,8 @@ func (statement *Statement) writeForUpdate(w *builder.BytesWriter) error {
 		return nil
 	}
 
-	if statement.dialect.URI().DBType != schemas.MYSQL {
-		return errors.New("only support mysql for update")
+	if statement.dialect.URI().DBType != schemas.MYSQL && statement.dialect.URI().DBType != schemas.POSTGRES {
+		return errors.New("only support mysql or postgres for update")
 	}
 	_, err := fmt.Fprint(w, " FOR UPDATE")
 	return err
